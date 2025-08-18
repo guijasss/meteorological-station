3160af9f13614418ea1ee87718ed3d91950fd4a2
Guilherme Samuel <guilhermesamuel79@gmail.com>
2025-08-18 16:27:30 -0300

    refact: format data outside function

--- o11y/app.py
+++ o11y/app.py
@@ -5,6 +5,8 @@ from tabulate import tabulate
 # Inicializa cliente Docker
 client = docker.from_env()
 
+DEFAULT_PRECISION = 2
+
 # Função para coletar métricas de todos os containers
 def get_container_metrics():
     containers = client.containers.list()
@@ -18,17 +20,17 @@ def get_container_metrics():
         system_delta = stats["cpu_stats"].get("system_cpu_usage", 0) - stats["precpu_stats"].get("system_cpu_usage", 0)
         percpu = stats["cpu_stats"]["cpu_usage"].get("percpu_usage", [])
         num_cpus = len(percpu) if percpu else 1
-        cpu_percent = (cpu_delta / system_delta) * num_cpus * 100.0 if system_delta > 0 else 0.0
+        cpu_percent = round((cpu_delta / system_delta) * num_cpus * 100.0, DEFAULT_PRECISION) if system_delta > 0 else 0.0
 
         # Memória
-        mem_used = stats["memory_stats"]["usage"] / (1024 * 1024)
-        mem_limit = stats["memory_stats"]["limit"] / (1024 * 1024)
-        mem_percent = (mem_used / mem_limit * 100) if mem_limit > 0 else 0.0
+        mem_used = round(stats["memory_stats"]["usage"] / (1024 * 1024), DEFAULT_PRECISION)
+        mem_limit = round(stats["memory_stats"]["limit"] / (1024 * 1024), DEFAULT_PRECISION)
+        mem_percent = round((mem_used / mem_limit * 100), DEFAULT_PRECISION) if mem_limit > 0 else 0.0
 
         # Rede
         networks = stats.get("networks", {})
-        rx = sum(n["rx_bytes"] for n in networks.values()) / (1024*1024)
-        tx = sum(n["tx_bytes"] for n in networks.values()) / (1024*1024)
+        rx = round(sum(n["rx_bytes"] for n in networks.values()) / (1024*1024), DEFAULT_PRECISION)
+        tx = round(sum(n["tx_bytes"] for n in networks.values()) / (1024*1024), DEFAULT_PRECISION)
 
         # I/O de bloco
         blkio = stats.get("blkio_stats", {}).get("io_service_bytes_recursive") or []
@@ -39,13 +41,16 @@ def get_container_metrics():
         restarts = c.attrs.get("RestartCount", 0)
 
         metrics_list.append({
-            "Container": c.name,
-            "CPU %": f"{cpu_percent:.2f}%",
-            "MEM USAGE / LIMIT": f"{mem_used:.2f}MiB / {mem_limit:.0f}MiB",
-            "MEM %": f"{mem_percent:.2f}%",
-            "NET I/O": f"{rx:.2f}MB / {tx:.2f}MB",
-            "BLOCK I/O": f"{io_read:.2f}MB / {io_write:.2f}MB",
-            "Restarts": restarts
+            "container": c.name,
+            "cpu_percent": cpu_percent,
+            "mem_usage": mem_used,
+            "mem_limit": mem_limit,
+            "mem_percent": mem_percent,
+            "rx_bytes": rx,
+            "tx_bytes": tx,
+            "io_read": io_read,
+            "io_write": io_write,
+            "restarts": restarts
         })
 
     return metrics_list
@@ -53,7 +58,19 @@ def get_container_metrics():
 # Função para exibir tabela no terminal
 def show_metrics():
     while True:
-        metrics = get_container_metrics()
+        metrics = [
+            {
+                "Container": m.get("container"),
+                "CPU %": f'{m.get("cpu_percent")}%',
+                "MEM USAGE / LIMIT": f'{m.get("mem_usage")} MiB / {m.get("mem_limit"):.0f}MiB',
+                "MEM %": f"{m.get('mem_percent')}%",
+                "NET I/O": f"{m.get('rx_bytes')}MB / {m.get('tx_bytes')}MB",
+                "BLOCK I/O": f"{m.get('io_read')}MB / {m.get('io_write')}MB",
+                "Restarts": m.get("restarts")
+            } for m in
+            get_container_metrics()
+        ]
+
         print("\033c", end="")  # limpa terminal
         print(f"📊 Métricas dos Containers - Atualizado em {time.strftime('%Y-%m-%d %H:%M:%S')}")
         print(tabulate(metrics, headers="keys", tablefmt="grid"))
